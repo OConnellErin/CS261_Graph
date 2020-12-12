9a96b36a27256af5b3d45984a47bd507ff22153f
Erin O'Connell <oconerin@oregonstate.edu>
2020-12-11 21:32:16 -0800

    passed test_neighbors

---
+++ graph.py
@@ -0,0 +1,16 @@
+# Graph: An efficient graph.
+# A graph implementation that uses an adjacency list to represent vertices
+# and edges.
+# Your implementation should pass the tests in test_graph.py.
+# Erin O'Connell    
+
+import functools
+
+class Graph:
+
+    def __init__(self):
+        self.data = {}
+        
+    def neighbors(self,vertex):
+        return []    
+
